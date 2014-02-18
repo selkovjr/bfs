0e03d7d00b929c02fd96529f9bfc42ea04d365ff
Gene Selkov <selkovjr@gmail.com>
2014-02-18 14:22:20 +0000

    added keyboard action to login

--- mojits/Login/binders/index.js
+++ mojits/Login/binders/index.js
@@ -1,5 +1,6 @@
-/*jslint anon:true, sloppy:true, nomen:true*/
-YUI.add('LoginBinderIndex', function(Y, NAME) {
+/*global YUI */
+/*jslint indent: 2*/
+YUI.add('LoginBinderIndex', function (Y, NAME) {
 
 /**
  * The LoginBinderIndex module.
@@ -7,48 +8,34 @@ YUI.add('LoginBinderIndex', function(Y, NAME) {
  * @module LoginBinderIndex
  */
 
+  /**
+   * Constructor for the LoginBinderIndex class.
+   *
+   * @class LoginBinderIndex
+   * @constructor
+   */
+  Y.namespace('mojito.binders')[NAME] = {
+
     /**
-     * Constructor for the LoginBinderIndex class.
-     *
-     * @class LoginBinderIndex
-     * @constructor
+     * Binder initialization method, invoked after all binders on the page
+     * have been constructed.
      */
-    Y.namespace('mojito.binders')[NAME] = {
+    init: function (mojitProxy) {
+      console.log('init');
+      this.mojitProxy = mojitProxy;
+    },
 
-        /**
-         * Binder initialization method, invoked after all binders on the page
-         * have been constructed.
-         */
-        init: function(mojitProxy) {
-            this.mojitProxy = mojitProxy;
-        },
-
-        /**
-         * The binder method, invoked to allow the mojit to attach DOM event
-         * handlers.
-         *
-         * @param node {Node} The DOM node to which this mojit is attached.
-         */
-        bind: function(node) {
-            var me = this;
-            this.node = node;
-            /**
-             * Example code for the bind method:
-             *
-             * node.all('dt').on('mouseenter', function(evt) {
-             *   var dd = '#dd_' + evt.target.get('text');
-             *   me.node.one(dd).addClass('sel');
-             *
-             * });
-             * node.all('dt').on('mouseleave', function(evt) {
-             *   
-             *   var dd = '#dd_' + evt.target.get('text');
-             *   me.node.one(dd).removeClass('sel');
-             *
-             * });
-             */
-        }
-
-    };
-
-}, '0.0.1', {requires: ['event-mouseenter', 'mojito-client']});
+    /**
+     * The binder method, invoked to allow the mojit to attach DOM event
+     * handlers.
+     *
+     * @param node {Node} The DOM node to which this mojit is attached.
+     */
+    bind: function (node) {
+      Y.one('body').on('key', function () {
+        Y.log('enter');
+        Y.one('#login-form').submit();
+      }, 'enter');
+    }
+  };
+}, '0.0.1', {requires: ['node', 'event', 'event-key', 'mojito-client']});
