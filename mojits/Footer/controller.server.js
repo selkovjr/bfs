f9f3dc9eba7781e36c5605cffd022438a45cb964
Gene Selkov <selkovjr@gmail.com>
2014-06-04 19:33:04 -0500

    added commit message to Footer

--- mojits/Footer/controller.server.js
+++ mojits/Footer/controller.server.js
@@ -11,6 +11,24 @@ YUI.add('Footer', function(Y, NAME) {
   * @class Controller
   * @constructor
   */
+  function Shell(cmd, args, options, cb_stdout, cb_end) {
+    var
+    spawn = require('child_process').spawn,
+    child = spawn(cmd, args, options),
+    me = this;
+
+    me.exit = 0;  // Send a cb to set 1 when cmd exits
+    me.stdout = '';
+
+    child.stdout.on('data', function (data) {
+      cb_stdout(me, data);
+    });
+
+    child.stdout.on('end', function () {
+      cb_end(me);
+    });
+  }
+
   Y.namespace('mojito.controllers')[NAME] = {
     /**
     * Method corresponding to the 'index' action.
@@ -19,9 +37,29 @@ YUI.add('Footer', function(Y, NAME) {
     * to the Mojito API.
     */
     index: function(ac) {
-      ac.done({
-        title: "Footer title"
-      });
+      var shell;
+
+      shell = new Shell(
+        'git', ['log', '-1', '--format=%cd / %ce / %s'],
+        {
+          cwd: '/home/bfs/src/bfs'
+        },
+        //'date', ['-u'],
+        function (me, data) {
+          me.stdout += data.toString();
+        },
+        function (me) {
+          me.exit = 1;
+        }
+      );
+
+      function report() {
+        ac.done({
+          date: shell.stdout
+        });
+      }
+
+      setTimeout(report, 250);
     }
   };
 }, '0.0.1', {requires: ['mojito']});
